356e0a6f2b5e7dec490c3170d3c287a06257dc81
M-Yasir-Hassan <144553833+M-Yasir-Hassan@users.noreply.github.com>
2024-12-14 18:23:13 +0500

    Slider
    Slider And Assets added

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,25 @@
 import React from 'react'
+
 import Navbar from '../../components/Navbar/Navbar'
 import Footer from '../../components/Footer/Footer'
 import Banner from '../../components/Lower_Banner/Lower_Banner'
+import Navbar from '../components/Navbar/Navbar'
+import Footer from '../components/Footer/Footer'
+import Slider from '../components/Slider/Slider'
+import ProductCategories from 'pages/components/ProductCategories/ProductCategories'
+import Stats from 'pages/components/Stats/Stats'
+import WhyChooseUs from 'pages/components/WhyChooseUs/WhyChooseUs'
+
 const Home = () => {
   return (
     <div>
       <Navbar />
       <Banner />
+<Slider></Slider>
+      <ProductCategories></ProductCategories>
+      <Stats></Stats>
+      <WhyChooseUs></WhyChooseUs>
+
       <Footer />
     </div>
   )
