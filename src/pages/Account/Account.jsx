627233c0a0d95167b7e401f21ccc4fb1d22d9d8c
SAMREEN-AKRAMM <126068139+SAMREEN-AKRAMM@users.noreply.github.com>
2024-12-16 12:45:52 +0500

    Update Account.jsx

--- src/pages/Account/Account.jsx
+++ src/pages/Account/Account.jsx
@@ -1,18 +1,47 @@
-import React from 'react'
-import TImage from 'components/TImage/TImage'
 import Navbar from 'components/Navbar/Navbar'
+import React, { useState } from 'react'
+
+import './Account.css'
 import Footer from 'components/Footer/Footer'
-const Account = () =>
-{
+import { AccountDetails, Addresses, Downloads, Orders } from './Components'
+
+function Account() {
+
+    const [selected, setSelected] = useState(0)
+
     return (
-        <>
+        <div>
             <Navbar />
-            <TImage
-                title="My Account"
-                description="Manage your profile, orders, and settings for a seamless shopping experience."
-            />
+
+            <div className='account-page-container'>
+                <div className='account-welcome-container'>
+                    <h3>Welcome to your Account</h3>
+
+                    <p style={{ width: '60%' }}>
+                        We're thrilled to have you here in your personal e-commerce haven. This is your central hub for managing all things related to your shopping experience.
+                    </p>
+                </div>
+
+                <div className='account-settings-container'>
+                    <ul className='account-components-buttons'>
+                        <li className={`account-component-button ${selected === 0 ? 'active' : ''}`} onClick={() => setSelected(0)}>Account Details</li>
+                        <li className={`account-component-button ${selected === 1 ? 'active' : ''}`} onClick={() => setSelected(1)}>Addresses</li>
+                        <li className={`account-component-button ${selected === 2 ? 'active' : ''}`} onClick={() => setSelected(2)}>Orders</li>
+                        <li className={`account-component-button ${selected === 2 ? 'active' : ''}`} onClick={() => setSelected(3)}>Downloads</li>
+                    </ul>
+
+                    <div className='account-settings-content'>
+                        {selected === 0 && <AccountDetails />}
+                        {selected === 1 && <Addresses />}
+                        {selected === 2 && <Orders />}
+                        {selected === 3 && <Downloads />}
+                    </div>
+
+                </div>
+            </div>
+
             <Footer />
-        </>
+        </div>
     )
 }
 
