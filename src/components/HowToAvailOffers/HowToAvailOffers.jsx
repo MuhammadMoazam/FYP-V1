9bd70ee4f481b242ee3e0f0dead8425fe82de34d
M-Yasir-Hassan <144553833+M-Yasir-Hassan@users.noreply.github.com>
2024-12-20 22:09:50 +0500

    Update Component

--- src/components/HowToAvailOffers/HowToAvailOffers.jsx
+++ src/components/HowToAvailOffers/HowToAvailOffers.jsx
@@ -6,48 +6,16 @@ const HowToAvailOffers = () => {
     <div className="offers-container">
       <h2 className="offers-title">How to Avail Offers</h2>
       <div className="timeline">
-        {/* Step 1 */}
-        <div className="timeline-item left">
-          <div className="timeline-content">
-            <h3>Step 1</h3>
-            <p>
-              Drop by our physical store at Urban Clothe to browse our latest
-              collections and get personalized style advice.
-            </p>
-          </div>
-          <div className="timeline-icon active-icon">
-            <span>🚀</span>
-          </div>
-        </div>
-
-        {/* Step 2 */}
-        <div className="timeline-item right">
-          <div className="timeline-icon active-icon">
-            <span>🚀</span>
-          </div>
-          <div className="timeline-content">
-            <h3>Step 2</h3>
-            <p>
-              Reach out by phone at +92 313 7265181 for quick assistance, order
-              inquiries, or help with sizing.
-            </p>
-          </div>
-        </div>
-
-        {/* Step 3 */}
-        <div className="timeline-item left">
-          <div className="timeline-content">
-            <h3>Step 3</h3>
-            <p>
-              Have a question or need support? Email us at{" "}
-              <a href="mailto:urbanclothe@gmail.com">urbanclothe@gmail.com</a>,
-              and we'll get back to you as soon as possible.
-            </p>
-          </div>
-          <div className="timeline-icon">
-            <span>🚀</span>
-          </div>
-        </div>
+        
+          <Stepper stepNo={1} stepText={<p>Drop by our physical store at Urban Clothe to browse our latest  collections and get personalized style advice.</p>} />
+          <Stepper stepNo={2} stepText={<p>Reach out by phone at +92 313 7265181 for quick assistance, orderinquiries, or help with sizing.</p>}/>
+          <Stepper stepNo={3} stepText={
+            <p>Have a question or need support? 
+                <a className="link-inside-text" href="mailto:urbanclothe@gmail.com" target="_blank" rel="noopener noreferrer"> Email Us</a>
+              ,and we'll get back to you as soon as possible.
+            </p>}/>
+        
+        
       </div>
       <div className="contact-button">
         <button>Get In Touch →</button>
@@ -56,4 +24,17 @@ const HowToAvailOffers = () => {
   );
 };
 
+const Stepper = ({stepNo, stepText}) => {
+  return (
+    <div className="timeline-item">
+      <div className="timeline-icon active-icon">
+        <span>🚀</span>
+      </div>
+      <div className="timeline-content">
+        <h3>Step {stepNo}</h3>
+        {stepText}
+      </div>
+    </div>
+  );
+};
 export default HowToAvailOffers;
