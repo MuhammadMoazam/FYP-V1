a611c8721a1c3b98596abfe0da8b6b37ce10473d
Muhammad Moazam <109450741+MuhammadMoazam@users.noreply.github.com>
2024-12-24 20:20:23 +0500

    Create Admin Dashboard

--- FrontEnd/src/App.js
+++ FrontEnd/src/App.js
@@ -1,43 +1,59 @@
-import React from 'react';
-import './index.css'
-import
-  {
-    BrowserRouter as Router,
-    Routes,
-    Route,
-  } from "react-router-dom";
-import About from './pages/About/About';
-import Shop from './pages/Shop/Shop';
-import Contact from './pages/Contact/Contact';
-import Blog from './pages/Blog/Blog';
-import Home from './pages/Home/Home';
-import Term from './pages/Terms/Terms';
-import Cart from './pages/Cart/Cart';
-import Legal from './pages/Legal/Legal';
-import Account from './pages/Account/Account';
-import Wishlist from './pages/Wishlist/Wishlist';
-import Checkout from './pages/Checkout/Checkout';
+import React, { useEffect } from "react";
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { useColorModes } from "@coreui/react";
+import "./scss/style.scss";
 
+// Containers
+import DefaultLayout from "./layout/DefaultLayout";
 
-const App = () =>
-{
+// Pages
+import Login from "./views/pages/login/Login";
+import Register from "./views/pages/register/Register";
+import Page404 from "./views/pages/page404/Page404";
+import Page500 from "./views/pages/page500/Page500";
+import Home from "./User/pages/Home/Home.jsx";
+
+const App = () => {
+  const { isColorModeSet, setColorMode } = useColorModes(
+    "coreui-free-react-admin-template-theme",
+  );
+  const storedTheme = useSelector((state) => state.theme);
+
+  useEffect(() => {
+    const urlParams = new URLSearchParams(window.location.href.split("?")[1]);
+    const theme =
+      urlParams.get("theme") &&
+      urlParams.get("theme").match(/^[A-Za-z0-9\s]+/)[0];
+    if (theme) {
+      setColorMode(theme);
+    }
+
+    if (isColorModeSet()) {
+      return;
+    }
+
+    setColorMode(storedTheme);
+  }, []); // eslint-disable-line react-hooks/exhaustive-deps
+
   return (
-
-    <Router>
+    <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/shop" element={<Shop />} />
-        <Route path="/blog" element={<Blog />} />
-        <Route path="/terms" element={<Term />} />
-        <Route path="/legal" element={<Legal />} />
-        <Route path="/cart" element={<Cart />} />
-        <Route path="/account" element={<Account />} />
-        <Route path="/wishlist" element={<Wishlist />} />
-        <Route path="/checkout" element={<Checkout />} />
+        {/* Default route redirects to /test */}
+        <Route path="/" element={<Navigate to="/test" replace />} />
+        <Route exact path="/test" name="Home" element={<Home />} />
+        <Route exact path="/login" name="Login Page" element={<Login />} />
+        <Route
+          exact
+          path="/register"
+          name="Register Page"
+          element={<Register />}
+        />
+        <Route exact path="/404" name="Page 404" element={<Page404 />} />
+        <Route exact path="/500" name="Page 500" element={<Page500 />} />
+        <Route path="*" name="Home" element={<DefaultLayout />} />
       </Routes>
-    </Router>
+    </BrowserRouter>
   );
 };
 
---
+++ FrontEnd/src/User/components/Stats/Stats.jsx
@@ -0,0 +1,28 @@
+/* eslint-disable prettier/prettier */
+import React from "react";
+import "./stats.css";
+
+const Stats = () => {
+  return (
+    <div className="stats-container">
+      <div className="stat">
+        <span className="number">2</span>
+        <span className="label">Years since start</span>
+      </div>
+      <div className="stat">
+        <span className="number">20</span>
+        <span className="label">Great employees</span>
+      </div>
+      <div className="stat">
+        <span className="number">+200</span>
+        <span className="label">Fantastic customers</span>
+      </div>
+      <div className="stat">
+        <span className="number">+100</span>
+        <span className="label">Reviews</span>
+      </div>
+    </div>
+  );
+};
+
+export default Stats;
